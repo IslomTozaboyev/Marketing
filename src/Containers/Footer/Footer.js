1d1b214f5ce79d714fa68b73d8ec39453d5b0ece
Islom <itozaboyev@gmail.com>
2022-02-03 00:55:52 +0500

    marketing-website

--- src/Containers/Footer/Footer.js
+++ src/Containers/Footer/Footer.js
@@ -26,8 +26,8 @@ const Footer = () => {
 
                     <div className="row">
                         {footer__links.length > 0 && footer__links.map((value, index) => {
-                            return <div className="col-12 col-md-4 mb-4 p-0">
-                                <Slide key={index} direction='up' duration={value.duration} >
+                            return <div key={index} className="col-12 col-md-4 mb-4 p-0">
+                                <Slide direction='up' duration={value.duration} >
                                     <a href={value.link} className='me-4 text-white footer__link'>{value.text}</a>
                                 </Slide>
                             </div>
